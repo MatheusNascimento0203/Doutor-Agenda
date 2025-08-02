9cb281c3c3cb8e82e1022019ecb9b1d44052e928
Matheus Nascimento <matheusfla.12333@gmail.com>
2025-08-01 22:49:43 -0300

    feat: add clinic form page

---
+++ src/app/clinic-form/page.tsx
@@ -0,0 +1,50 @@
+import { eq } from "drizzle-orm";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { db } from "@/db";
+import { usersToClinicsTable } from "@/db/schema";
+import { auth } from "@/lib/auth";
+
+import ClinicForm from "./components/form";
+
+const ClinicFormPage = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session?.user) {
+    redirect("/authentication");
+  }
+
+  const clinics = await db.query.usersToClinicsTable.findMany({
+    where: eq(usersToClinicsTable.userId, session.user.id),
+  });
+
+  if (clinics.length > 0) {
+    redirect("/dashboard");
+  }
+
+  return (
+    <Dialog open>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Adicionar Clinica</DialogTitle>
+          <DialogDescription>
+            Adicione uma nova clinica para continuar.
+          </DialogDescription>
+        </DialogHeader>
+        <ClinicForm />
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default ClinicFormPage;
