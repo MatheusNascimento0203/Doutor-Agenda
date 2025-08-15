e168f10eb2666ff547a2b7b273758f84a3ac0cae
Matheus Nascimento <matheusfla.12333@gmail.com>
2025-08-15 15:01:42 -0300

    feat: implement data table for patient management and update dependencies

---
+++ src/app/(protected)/patients/_components/table-columns.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+import { MoreVerticalIcon } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { patientsTable } from "@/db/schema";
+
+type Patient = typeof patientsTable.$inferSelect;
+
+export const patientsTableColumns: ColumnDef<Patient>[] = [
+  {
+    id: "name",
+    accessorKey: "name",
+    header: "Nome",
+  },
+  {
+    id: "email",
+    accessorKey: "email",
+    header: "E-mail",
+  },
+  {
+    id: "phone_number",
+    accessorKey: "phone_number",
+    header: "Telefone",
+  },
+  {
+    id: "sex",
+    accessorKey: "sex",
+    header: "Sexo",
+    cell: (params) => {
+      const patient = params.row.original;
+      return patient.sex === "male" ? "Masculino" : "Feminino";
+    },
+  },
+  {
+    id: "actions",
+    cell: () => {
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <Button variant="ghost" size="icon">
+              <MoreVerticalIcon className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Profile</DropdownMenuItem>
+            <DropdownMenuItem>Billing</DropdownMenuItem>
+            <DropdownMenuItem>Team</DropdownMenuItem>
+            <DropdownMenuItem>Subscription</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  },
+];
--- src/app/(protected)/patients/page.tsx
+++ src/app/(protected)/patients/page.tsx
@@ -1,3 +1,8 @@
+import { eq } from "drizzle-orm";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+import { DataTable } from "@/components/ui/data-table";
 import {
   PageActions,
   PageContainer,
@@ -7,10 +12,30 @@ import {
   PageHeaderContent,
   PageTitle,
 } from "@/components/ui/page-container";
+import { db } from "@/db";
+import { patientsTable } from "@/db/schema";
+import { auth } from "@/lib/auth";
 
 import AddPatientButton from "./_components/add-patient-button";
+import { patientsTableColumns } from "./_components/table-columns";
 
-const PatientPage = () => {
+const PatientPage = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session?.user) {
+    redirect("/authentication");
+  }
+
+  if (!session.user?.clinic) {
+    redirect("/clinic-form");
+  }
+
+  const patients = await db.query.patientsTable.findMany({
+    where: eq(patientsTable.clinicId, session.user.clinic.id),
+  });
+
   return (
     <PageContainer>
       <PageHeader>
@@ -25,9 +50,7 @@ const PatientPage = () => {
         </PageActions>
       </PageHeader>
       <PageContent>
-        <div>
-            {/* GRID OF PATIENTS */}
-        </div>
+        <DataTable data={patients} columns={patientsTableColumns} />
       </PageContent>
     </PageContainer>
   );
