6f329ddefcb0ab972a72c407821c965e8dfbfa90
Matheus Nascimento <matheusfla.12333@gmail.com>
2025-08-19 14:58:46 -0300

    feat: add patients table

---
+++ src/app/(protected)/patients/_components/table-columns.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+import { EditIcon, MoreVerticalIcon, TrashIcon } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { patientsTable } from "@/db/schema";
+
+type Patient = typeof patientsTable.$inferSelect;
+
+export const patientsTableColumns: ColumnDef<Patient>[] = [
+  {
+    id: "name",
+    accessorKey: "name",
+    header: "Nome",
+  },
+  {
+    id: "email",
+    accessorKey: "email",
+    header: "E-mail",
+  },
+  {
+    id: "phone_number",
+    accessorKey: "phone_number",
+    header: "Telefone",
+  },
+  {
+    id: "sex",
+    accessorKey: "sex",
+    header: "Sexo",
+    cell: (params) => {
+      const patient = params.row.original;
+      return patient.sex === "male" ? "Masculino" : "Feminino";
+    },
+  },
+  {
+    id: "actions",
+    cell: (params) => {
+      const patient = params.row.original;
+      const partes = patient.name.trim().split(" ").filter(Boolean);
+      const primeiroNome = partes[0] || "";
+      const sobrenome = partes[1] || "";
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <Button variant="ghost" size="icon" className="cursor-pointer">
+              <MoreVerticalIcon className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>
+              {primeiroNome} {sobrenome}
+            </DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+              <EditIcon />
+              Editar
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <TrashIcon />
+              Excluir
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  },
+];
